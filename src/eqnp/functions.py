6aabb3990d71cca126d70c764870a331ebd67edb
Kian Kasad <kian@kasad.com>
2022-02-24 21:10:29 -0800

    Add absolute value function

--- src/eqnp/functions.py
+++ src/eqnp/functions.py
@@ -15,7 +15,40 @@ from .expressions import *
 class Function(UnaryExpression, ABC):
     pass
 
+class AbsoluteValue(Function):
+    def __repr__(self):
+        return f'|{self.value}|'
+
+    def evaluate(self, vm: VariableMap = None):
+        return abs(self.value.evaluate(vm))
+
+    def differentiate(self, respectTo: str, vm: VariableMap = None):
+        return Multiplication(
+            Division(self, self.value),
+            self.value.differentiate(respectTo, vm)
+        )
+
+    def simplify(self):
+        self.value = self.value.simplify()
+
+        # For constants > 0, return the constant
+        if isinstance(self.value, Number) and self.value.evaluate() >= 0:
+            return self.value
+
+        # Even exponents are never negative
+        if isinstance(self.value, Exponent) \
+                and isinstance(self.value.right, Number) \
+                and self.value.right.evaluate() % 2 == 0:
+            return self.value
+
+        return self
+
+# Alias for AbsoluteValue
+ABS = AbsoluteValue
+
 # Define exports
 __all__ = [
+    'ABS',
+    'AbsoluteValue',
     'Function',
 ]
